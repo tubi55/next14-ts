a1e85ac182ebd4840b9b170083a9cb0d5b2a3743
tubi55 <tubi55@nate.com>
2024-03-02 16:04:01 +0900

    clerk인증 페이지 이동처리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
-import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { IBM_Plex_Sans } from 'next/font/google';
+import './globals.css';
 //shadcn을 설치함으로써 자동으로 설치됨 clsx 설치할 필요 없음
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const IBMPlex = IBM_Plex_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-ibm-plex",
+	subsets: ['latin'],
+	weight: ['400', '500', '600', '700'],
+	variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
-  title: "Imaginify",
-  description: "AI-powered Image generator",
+	title: 'Imaginify',
+	description: 'AI-powered Image generator'
 };
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<ClerkProvider>
+			<html lang='en'>
+				<body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
 }
