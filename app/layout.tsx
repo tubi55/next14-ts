1da5f3ae3e8e013446e7986bbd4cfaad07c91b40
tubi55 <tubi55@nate.com>
2024-03-04 17:43:00 +0900

    page.jsx 파일 (root)폴더 안쪽으로 이동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
 import { IBM_Plex_Sans } from 'next/font/google';
-import './globals.css';
 //shadcn을 설치함으로써 자동으로 설치됨 clsx 설치할 필요 없음
 import { cn } from '@/lib/utils';
 import { ClerkProvider } from '@clerk/nextjs';
+import './globals.css';
 
 const IBMPlex = IBM_Plex_Sans({
 	subsets: ['latin'],
