b0437c4fe26131cfab5b87530a365c76d7a93b73
tubi55 <tubi55@nate.com>
2024-03-06 15:50:06 +0900

    vercel 배포오류 수정 시도 (prettier애서 single quote기능 해제)

--- types/index.d.ts
+++ types/index.d.ts
@@ -88,7 +88,7 @@ declare type CreateTransactionParams = {
 	createdAt: Date;
 };
 
-declare type TransformationTypeKey = 'restore' | 'fill' | 'remove' | 'recolor' | 'removeBackground';
+declare type TransformationTypeKey = "restore" | "fill" | "remove" | "recolor" | "removeBackground";
 
 // ====== URL QUERY PARAMS
 declare type FormUrlQueryParams = {
@@ -114,7 +114,7 @@ declare type SearchParamProps = {
 };
 
 declare type TransformationFormProps = {
-	action: 'Add' | 'Update';
+	action: "Add" | "Update";
 	userId: string;
 	type: TransformationTypeKey;
 	creditBalance: number;
